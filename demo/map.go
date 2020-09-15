ddb62999e3900d3fcf86c50bb3453b1ed1619a13
chixin.xie <chixin.xie@pitayagames.com>
2020-09-15 16:48:54 +0800

    master

--- demo/map.go
+++ demo/map.go
@@ -1,37 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"unsafe"
-)
-
 func main() {
-	m := make(map[string]string, 1)
-	fmt.Println(len(m))
-	var change = func(m map[string]string) {
-		m = make(map[string]string, 2)
-	}
-
-	change(m)
-	fmt.Println(len(m))
-
-	s := make([]int, 0, 1)
-	s = append(s, 1)
-	fmt.Println(s)
-	fmt.Println(cap(s))
-	var changeSlice = func(s []int) []int {
-		fmt.Println(unsafe.Pointer(&s))
-		s = append(s, 2)
-		return s
-	}
-	fmt.Println(unsafe.Pointer(&s))
-	s = changeSlice(s)
-	fmt.Println(unsafe.Pointer(&s))
-	fmt.Println(s)
-	fmt.Println(cap(s))
-
-}
-
-func feather1() {
 
 }
